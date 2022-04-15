b84056c72f3dd5efd8428959a596bd01eaf737bd
yixiu <yixiu@gaoding.com>
2022-04-15 18:12:44 +0800

    remove useless

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,123 @@
-// routeA = route('topic/:id', async (request) => {
-//     return {};
-// });
+import { deepmerge } from "deepmerge-ts";
+export const merge = deepmerge;
 
-// { topic: { ':id': (request: Req) => Resp } }
-
-// allRoutes = merge(routeA)
-
-// { topic: { ':id': (request: Req) => Resp } }
-
-// service = setup(allRoutes)
-
-// service.get('topic/123') // => Promise<Resp>;
-
-// { topic: { ':id': (request: Req) => Resp } } => 'topic/$': (request: Req) => Resp
-// 'topic/123' => 'topic/$'
-
-
-
-
-type Method = 'GET' | 'POST';
-
-const method = ['GET', 'POST'];
-
-type RestFulApi = {
-    get: () => void;
-    // fork: () => void; // 增加中间件
+export function defineRoute<
+  M extends Method,
+  R extends string,
+  Handler extends (request: any) => any
+>(method: M, route: R, cb: Handler): PathRecord<M, RemoveSlash<R>, Handler> {
+  throw new Error("not implement");
 }
 
-type TestUrl = '/topic/123'; // expected match to 'topic/$', '$/123', '$/$', 'topic/123'
+export function createService<T extends Record<string, any>>(
+  routes: T
+): Service<T> {
+  throw new Error("not implement");
+}
 
-// type Split<T extends string, S extends string> = T extends `${infer L}${S}${infer R}` ? L | Split<R, S> : T;
-// type SplitTest = Exclude<Split<TestUrl, '/'>, ''>;
+// const routeA: {
+//     topic: {
+//         ":id": {
+//             GET: () => void;
+//         };
+//     };
+// }
+const route1 = defineRoute("GET", "/topic/:id", (request: string) => {
+  return 123;
+});
+const route2 = defineRoute("POST", "/topic/:id", () => {});
+const route3 = defineRoute("GET", "/topic", () => {});
+const route4 = defineRoute("GET", "/file", (req?: {}) => {});
 
-type Split<T extends string, S extends string> = T extends `${infer L}${S}${infer R}` ? L extends '' ? [...Split<R, S>] : [L, ...Split<R, S>] : [T];
-type SplitTest = Split<TestUrl, '/'>;
+const routes = deepmerge(route1, route2, route3, route4);
 
-type Pattern<T extends string> = T extends `${infer L}/${infer R}` ? T | `$/${Pattern<R>}` | `${L}/${Pattern<R>}` : T | '$';
-type PatternTest = Pattern<'topic/123'>;
+const service = createService(routes);
+service.run("GET", "/file", undefined);
+service.run("GET", "/topic/123", "123").then((v) => {});
+
+/** type utils */
+type MapTopParameter<U> = U extends any ? (arg: U) => void : never;
+
+type IntersectionFromUnion<U> = MapTopParameter<U> extends (
+  arg: infer T
+) => void
+  ? T
+  : never;
+
+type PickMethod<T, K = Extract<T, Method>> = IntersectionFromUnion<
+  K extends keyof T
+    ? {
+        [P in K]: T[P];
+      }
+    : never
+>;
+
+type Convert$<T extends string> = T extends `:${infer S}` ? "$" : T;
 type RemoveSlash<T extends string> = T extends `/${infer S}` ? S : T;
-type RemoveSlashTest = RemoveSlash<'/topic/123'>; // 'topic/123'
 type AddSlash<T extends string> = T extends `/${infer S}` ? T : `/${T}`;
-type AddSlashTest = AddSlash<'topic/123'>; // 'topic/123'
+
+/** type function */
+
+type Method = "GET" | "POST";
+
+type PathRecord<
+  M extends Method,
+  T extends string,
+  V extends any
+> = T extends `${infer L}/${infer R}`
+  ? { [P in L]: PathRecord<M, R, V> }
+  : { [P in T]: { [K in M]: V } };
+
+type Pattern<T extends string> = T extends `${infer L}/${infer R}`
+  ? T | `$/${Pattern<R>}` | `${L}/${Pattern<R>}`
+  : T | "$";
 type RoutePattern<T extends string> = AddSlash<Pattern<RemoveSlash<T>>>;
 // type RoutePatternTest = RoutePattern<'/topic/123'>;
+// type RoutePatternTest = "/topic/123" | "/$/123" | "/$/$" | "/topic/$"
 
-// ["a", "b"] => { a: { b: any } }
-// type PathRecord<T extends string, V extends any> = T extends `${infer L}/${infer R}` ? Record<L, PathRecord<R, V>> : {[P in T]: V;};
-type PathRecord<M extends Method, Route extends string, V extends any, T = RemoveSlash<Route>> = T extends `${infer L}/${infer R}` ? {[P in L]: PathRecord<M, R, V>;}  : {[P in Route]: { [K in M]: V };};
-type PathRecordTest = PathRecord<'GET', '/topic/:id', any>;
-
-// type TestRoute = '/topic/:id'; // expected to be 'topic/$'
-type RestfulRouter<M extends Method, Route extends string, Req extends any, Resp extends any> = never; // TODO: RestfulRouter
-
-type RestFulCall<M extends Method, URL extends string> = (method: M, url: URL) => void;
-
-// export function createRestfulApi() {
-//     return {}
-// }
-
-export {};
-
-
-
-
-type TestRouter = {
-    'topic': {
-        'GET': (request: undefined) => Promise<{ id: string, title: string }[]>,
-        ':id': {
-            'GET': (request: { id: string }) => Promise<{ id: string, title: string }>,
-            'POST': (request: { title: string }) => Promise<{ id: string, title: string }>,
-        }
-    },
-    'file': {
-        'GET': (request: undefined) => Promise<{ id: string, name: string }[]>,
-    }
-}
-type Map<T extends string, V extends any = any> = { [P in T]: { [K in P]: V } }[T]
-
-type MapTopParameter<U> = U extends any ? (arg: U) => void : never
-type IntersectionFromUnion<U> =
-  MapTopParameter<U> extends (arg: infer T) => void ? T : never
-type PickMethod<T, K = Extract<T, Method>> = IntersectionFromUnion<K extends keyof T ? {
-    [P in K]: T[P];
-} : never>;
-
-type Convert$<T extends string> = T extends `:${infer S}` ? '$' : T;
-
-type Regenerate<
-    T extends Record<string, any>,
-    L extends string | number = '',
-    Sub = Omit<T, Method>,
-    Ms = PickMethod<T, Method>
+// convert to be { 'topic': { 'GET': xxx }; 'topic/$': { 'GET': xxx, 'POST': xxx }; 'file': {'GET': xxx }; }
+type Flatten<
+  T extends Record<string, any>,
+  L extends string | number = "",
+  Sub = Omit<T, Method>,
+  Ms = PickMethod<T, Method>
 > = Extract<keyof T, Method> extends never
-    ? { [K in keyof Sub]: K extends string ? Regenerate<Sub[K], `${L}/${Convert$<K>}`> : never }[keyof Sub]
-    : { [LK in L]: Ms} | { [K in keyof Sub]: K extends string ? Regenerate<Sub[K], `${L}/${Convert$<K>}`> : never }[keyof Sub];
-// expect to be ['topic', { 'GET': xxx }] | ['topic/$', { 'GET': xxx, 'POST': xxx }] | ['file', {'GET': xxx}]
-type RegenerateTest = IntersectionFromUnion<Regenerate<TestRouter>>;
-type RegenerateTest2 = Regenerate<TestRouter['topic'], 'topic'>;
+  ? {
+      [K in keyof Sub]: K extends string
+        ? Flatten<Sub[K], `${L}/${Convert$<K>}`>
+        : never;
+    }[keyof Sub]
+  :
+      | { [LK in L]: Ms }
+      | {
+          [K in keyof Sub]: K extends string
+            ? Flatten<Sub[K], `${L}/${Convert$<K>}`>
+            : never;
+        }[keyof Sub];
+type FlattenRoutes<T extends Record<string, any>> = IntersectionFromUnion<
+  Flatten<T>
+>;
 
-type RegenerateTest3 = Omit<TestRouter['topic'], Method>;
-type RegenerateTest4 = PickMethod<TestRouter, Method>;
-type RegenerateTest5 = PickMethod<TestRouter['topic'], Method>;
-type RegenerateTest7 = PickMethod<TestRouter['topic'][':id'], Method>;
-type RegenerateTest6 = Extract<keyof TestRouter, Method>;
+/** type defination */
 
-type RoutePatternTest = RoutePattern<'/topic/123'>;
+type Service<T extends Record<string, any>, FRoutes = FlattenRoutes<T>> = {
+  run<
+    M extends keyof Handlers,
+    R extends Overlap extends never ? never : string,
+    Req extends Handler extends (...args: any) => any
+      ? Parameters<Handler>[0]
+      : never,
+    Resp extends Handler extends (...args: any) => any
+      ? ReturnType<Handler>
+      : never,
+    Overlap = RoutePattern<R> & keyof FRoutes,
+    Handlers = Overlap extends keyof FRoutes ? FRoutes[Overlap] : never,
+    Handler = Handlers[M]
+  >(
+    method: M,
+    url: R,
+    request: Req
+  ): Promise<Resp>;
+};
 
-type IfRouteMatch = keyof RegenerateTest & RoutePatternTest extends never ? false : true;
-type Overlap = keyof RegenerateTest & RoutePatternTest;
-type Matcher = RegenerateTest[Overlap];
-
-
-function defineRoute<M extends Method, R extends string, Cb extends (request: any) => any>(method: M, route: R, cb: Cb): PathRecord<M, R, Cb> {
-    throw new Error('not implement');
-}
-
-const routeA = defineRoute('GET', '/topic/:id', () => {});
+type test = FlattenRoutes<typeof routes>;
